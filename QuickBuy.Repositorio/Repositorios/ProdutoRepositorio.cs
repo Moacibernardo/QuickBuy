470bc3f51016cd1bd92e2d65c4b5a46091415c99
Moacibernardo <mjbernardoe@yahoo.com.br>
2020-12-21 10:11:52 -0300

    Inclusão de Repositorios

---
+++ QuickBuy.Repositorio/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,8 @@
+﻿using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.Contratos;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public  class ProdutoRepositorio : BaseRepositorio<Produto>, IProdutoRepositorio
+
+}
