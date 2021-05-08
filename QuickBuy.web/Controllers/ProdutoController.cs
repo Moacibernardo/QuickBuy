1476b77ecdc047f18110e018a6bff269837b7712
Moacibernardo <mjbernardoe@yahoo.com.br>
2021-05-08 15:28:17 -0300

    Criando Produto Controller

---
+++ QuickBuy.web/Controllers/ProdutoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+    }
+}
