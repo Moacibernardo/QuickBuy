f07d159eb87cc696f4a2a500cac9468e8e444665
Moacibernardo <mjbernardoe@yahoo.com.br>
2021-05-10 13:44:11 -0300

    Adicionando metodos na WebApi

--- QuickBuy.web/Controllers/ProdutoController.cs
+++ QuickBuy.web/Controllers/ProdutoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using System;
 
 namespace QuickBuy.Web.Controllers
 {
@@ -11,5 +13,38 @@ namespace QuickBuy.Web.Controllers
         {
             _produtoRepositorio = produtoRepositorio;
         }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());
+                //if(condicao == false)
+                //{
+                //    return BadRequest("")
+                //}
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+
+            }
+        }
+            [HttpPost]
+            public IActionResult Post([FromBody]Produto produto)
+            {
+                try
+                {
+                   _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.ToString());
+                }
+            }
+        
     }
 }
