9c581f8bfe0559b94a8a1e0860aac3acec72497e
Moacibernardo <mjbernardoe@yahoo.com.br>
2021-05-10 15:34:00 -0300

    Ajustes para compliar e testar WebApi

--- QuickBuy.Repositorio/Repositorios/PedidoRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/PedidoRepositorio.cs
@@ -1,9 +1,13 @@
 ﻿using QuickBuy.Dominio.Entidades;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
-    public  class PedidoRepositorio : BaseRepositorio<Pedido>, IPedidoRepositorio
+    public class PedidoRepositorio : BaseRepositorio<Pedido>, IPedidoRepositorio
     {
+        public PedidoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
+        {
+        }
     }
 }
