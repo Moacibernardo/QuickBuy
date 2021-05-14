5cf7641402de70756d27029dd710607189086225
Moacibernardo <mjbernardoe@yahoo.com.br>
2021-05-14 14:01:29 -0300

    Adicionando componente Angular

---
+++ QuickBuy.web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "produto",
+  template : "<html><body>{{ obterNome }}}</body></html>"
+})
+export class ProdutoComponent {// Nome dad classes começando com maísuculo por conta da convenção PascalCase
+/* camelCase para variáveis. atributos e nomes das funções */
+
+  public nome: string;
+  public liberadoParavenda: boolean;
+
+  public obterNome(): string {
+    return "Samsung";
+
+  }
+
+
+}
